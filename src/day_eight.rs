e9915312a567408b7dccd43511a936d934fb2643
Kirsty <kirstysays@gmail.com>
2020-12-11 08:16:01 +0000

    Day eight part one completed

--- src/day_eight.rs
+++ src/day_eight.rs
@@ -3,49 +3,49 @@ use std::fs;
 pub struct Instructions
 {
     pub instr : String,
-    pub value : usize,
+    pub value : i64,
     pub completed : bool
 }
 
 pub fn run()
 {
     let mut instructions = get_instruction_set();
-    let mut total : usize = 0;
-    let mut current :usize = 0;
+    let mut total : i64 = 0;
+    let mut current :i64 = 0;
 
     loop 
     {
-        let line = &instructions[current];
+        let current_u : usize = current as usize;
+        if instructions[current_u].completed
+        {
+           // We have found our answer to part one.
+           println!("Part One answer is : {:?}", total);
+           break;
+        }
+        
+        instructions[current_u].completed = true;
+        let line = &instructions[current_u];
         let instr = &line.instr;
         let value = &line.value;
 
-        instructions[current].completed = true;
-
+    
         if instr.contains("acc")
         {
             // Change the total by the value
-            println!("matched with acc");
             total += value;
             current += 1;
         }
         else if instr.contains("jmp")
         {
             // Jump to the next set of instructions 
-            println!("matched with jmp");
-            current += value; 
+            current += value ; 
         }
         else if instr.contains("nop")
         {
             // Move to the next instruction
-            println!("matched with nop");
             current += 1;
-        }
-
-        
-    
+        }    
     }
-
-
 }
 
 fn get_instruction_set() -> Vec<Instructions>
@@ -58,16 +58,10 @@ fn get_instruction_set() -> Vec<Instructions>
     for value in collection
     {
         let mut iter = value.trim().split_whitespace();
-        let temp_tuple : (String, usize, bool) = (iter.next().unwrap().to_string(), iter.next().unwrap().to_string().parse().unwrap_or_default(), false);
-        
-        let instruc : Instructions = Instructions{instr : temp_tuple.0, value : temp_tuple.1, completed : temp_tuple.2};
+        let instruc : Instructions = Instructions{instr : iter.next().unwrap().to_string(), value : iter.next().unwrap().to_string().parse().unwrap_or_default(), completed : false};
         instructions_vec.push(instruc);
-
     }
 
     instructions_vec
     
 }
-
-
-
